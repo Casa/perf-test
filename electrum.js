98a7986126ca4aed881be236685002c17fe650a7
Alex Miller <alex@team.casa>
2020-05-19 23:49:50 -0400

    config module

--- electrum.js
+++ electrum.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
+const config = require('./lib/config').parse(process.argv)
 const ElectrumCli = require('electrum-client')
 const bitcoin = require('bitcoinjs-lib')
-const yargs = require('yargs')
 const fs = require('fs')
 
 // Constants
@@ -17,88 +17,21 @@ const TESTNET_P2SH_P2WSH = {
   wif: 0xef,
 };
 
-// Parse CLI parameters
-const argv = yargs
-  .group(['addr'], 'Input options:')
-  .option('addr', {
-    alias: 'a',
-    description: 'Address file',
-    type: 'string'
-  })
-  .group(['compact','quiet'], 'Output options:')
-  .option('compact', {
-    description: 'Compact output, line per address',
-    type: 'boolean',
-    default: false
-  })
-  .option('quiet', {
-    alias: 'q',
-    description: 'Suppress header & footer output',
-    type: 'boolean',
-    default: false
-  })
-  .group(['get_balance','get_history','listunspent'], 'Tests to run:')
-  .option('get_balance', {
-    description: 'Measure balance lookup',
-    type: 'boolean',
-    default: false
-  })
-  .option('get_history', {
-    descrption: 'Measure total inputs',
-    type: 'boolean',
-    default: false
-  })
-  .option('listunspent', {
-    description: 'Measure UTXOs',
-    type: 'boolean',
-    default: false
-  })
-  .group(['host','port','ssl','testnet'], 'Electrum server options:')
-  .option('host', {
-    alias: 'H',
-    description: 'Server hostname or IP',
-    type: 'string',
-    default: '127.0.0.1'
-  })
-  .option('port', {
-    alias: 'P',
-    description: 'Server port',
-    type: 'string',
-    default: '50001'
-  })
-  .option('ssl', {
-    alias: 's',
-    description: 'Use SSL',
-    type: 'boolean',
-    default: false
-  })
-  .option('testnet', {
-    alias: 't',
-    description: 'Use bitcoin testnet',
-    type: 'boolean',
-    default: false
-  })
-  .usage('Usage: $0 [args]')
-  .help()
-  .alias('help', 'h')
-  .showHelpOnFail(true, 'Specify --help for available options')
-  .argv;
-
 // Update configuration
-quietOutput = argv.quiet;
-compactOutput = argv.compact;
+quietOutput = config.output_quiet;
+compactOutput = config.output_compact;
 
-electrumHost = argv.host;
-electrumNetwork = (argv.testnet) ? 'testnet' : 'mainnet';
-electrumPort = setPort(electrumNetwork, argv.ssl, argv.port);
+electrumHost = config.host;
+electrumNetwork = (config.testnet) ? 'testnet' : 'mainnet';
+electrumPort = setPort(electrumNetwork, config.ssl, config.port);
 electrumProto = 'tcp'
-electrumSsl = argv.ssl;
+electrumSsl = config.ssl;
 
-addresses = readAddressFile(argv.addr);
+addresses = readAddressFile(config.addr);
 
-testBalance = argv.get_balance
-testHistory = argv.get_history
-testUnspent = argv.listunspent
+testBalance = config.get_balance
+testHistory = config.get_history
+testUnspent = config.listunspent
 testAll = (testBalance || testHistory || testUnspent) ? false : true;
 
 

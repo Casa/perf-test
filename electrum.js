88ba3744ef36d9d1d07098c03e893eb02c2572a1
Alex Miller <alex@team.casa>
2020-05-22 00:53:34 -0400

    initial attempt at socket pool w/iteration

--- electrum.js
+++ electrum.js
@@ -17,7 +17,7 @@ electrumSsl = config.ssl;
 
 socketCount = config.socket_count;
 socketPool = [];
-socketPromise = [];
+socketPromises = [];
 
 let addresses = [];
 const ADDR_MAINNET = ['1AGyaDKdHWo8TcGADUCWd8JYXMQrky8Uko','3EBaaBxgShLxq8w2dDjhSfeb476wRScjKK'];
@@ -43,91 +43,64 @@ if (verbose > 2) console.log('measurement:', measurement);
 // Main
 
 const main = async () => {
-  if (!quiet) console.error(`\nConnecting to Electrum server: ${electrumHost}:${electrumPort}`);
-  const ecl = new ElectrumCli(electrumPort, electrumHost, electrumProto);
-  try {
+
+  // Create socket pool
+  for (let i = 0; i < socketCount; i++) {
+    const ecl = new ElectrumCli(electrumPort, electrumHost, electrumProto);
+    if (!quiet) console.error(`\nConnecting to Electrum server: ${electrumHost}:${electrumPort}`);
+    try {
       await ecl.connect();
-  } catch (e) {
+    } catch (e) {
       console.error('Error connecting:', e);
       process.exit(1);
-  }
-  try {
-    let addrTotal = addresses.length;
-    let addrErr = [];
-
-    const ver = await ecl.server_version("CasaPerfTest", "1.4");
-    const fee = await ecl.blockchainEstimatefee(4)
+    }
     if (verbose) {
+      const ver = await ecl.server_version("CasaPerfTest", "1.4");
+      const fee = await ecl.blockchainEstimatefee(4)
       console.error(`  server version: ${ver}`);
       console.error(`  fee estimate: ${fee}`)
     }
     if (!quiet) console.error();
-
-    let header = 'address'
-    if (measurement.get_balance || measurement.all) header += ',get_balanceConfirmed,get_balanceUnconfirmed,get_balanceTime';
-    if (measurement.get_history || measurement.all) header += ',get_historyCount,get_historyTime';
-    if (measurement.listunspent || measurement.all) header += ',listunspentCount,listunspentTime';
-    console.log(header)
-
-    while (addresses.length) {
-      address = addresses.pop();
-      if (address == '') continue;
-      //if (verbose > 1) console.log(`\naddress: ${address}`);
-
-      //const scriptHash = getScriptHash(address);
-      //let result = 'address';
-
-      //try {
-        //if (testBalance || testAll ) {
-        //  const get_balanceStart = new Date()
-        //  const get_balance = await ecl.blockchainScripthash_getBalance(scriptHash)
-        //  const get_balanceTime = new Date() - get_balanceStart
-        //  result += ',' + get_balanceTime
-        //  if (verbose > 1) {
-        //    console.log('get_balance result:', get_balance);
-        //    console.log('get_balance time:', get_balanceTime);
-        //  }
-        //}
-        //if (testHistory || testAll) {
-        //  const get_historyStart = new Date();
-        //  const get_history = await ecl.blockchainScripthash_getHistory(scriptHash)
-        //  const get_historyTime = new Date() - get_historyStart;
-        //  result += address + ',' + get_history.length + ',' + get_historyTime
-        //  if (verbose > 1) {
-        //    console.log(`get_history count: ${get_history.length}`);
-        //    console.log(`get_history time: ${get_historyTime}`);
-        //  }
-        //}
-        //if (testUnspent || testAll) {
-        //  const listunspentStart = new Date();
-        //  const unspent = await ecl.blockchainScripthash_listunspent(scriptHash)
-        //  const listunspentTime = new Date() - listunspentStart;
-        //  result += ',' + unspent.length + ',' + listunspentTime
-        //  if (verbose > 1) {
-        //    console.log(`listunspent count: ${unspent.length}`);
-        //    console.log(`listunspent time: ${listunspentTime}`);
-        //  }
-        //}
-        //if (verbose < 2) console.log(result);
-      //} catch(e) {
-      //  addrErr.push(e);
-      //}
-      await run_measurement(ecl, address, measurement);
-      //.then(
-      //  result => console.log(result),
-      //  error => console.error(error)
-      //);
-    }
-
-    if (!quiet) {
-      console.error(`\nAddresses tested: ${addrTotal}`);
-      console.error(`Addresses errored: ${addrErr.length}\n`);
-    }
-
-  } catch(e) {
-    console.error(e)
+    socketPool.push(ecl);
   }
-  await ecl.close()
+
+  // Iterate over addresses & sockets
+  let addrTotal = addresses.length;
+  let addrErr = 0;
+  while (addresses.length > 0) {
+    for (let i = 0; i < socketPool.length; i++) {
+      try {
+        let address = addresses.pop();
+        socketPromises.push(run_measurement(socketPool[i], address, measurement));
+        if (addresses.length == 0) break;
+      } catch (e) {
+        addrErr++;
+        if (verbose > 2) console.error('addr error:', e);
+      }
+    }
+  }
+  if(verbose) console.log(`Created ${socketPromises.length} promises. Now waiting for them to finish`);
+
+  // Print header
+  let header = 'address'
+  if (measurement.get_balance || measurement.all) header += ',get_balanceConfirmed,get_balanceUnconfirmed,get_balanceTime';
+  if (measurement.get_history || measurement.all) header += ',get_historyCount,get_historyTime';
+  if (measurement.listunspent || measurement.all) header += ',listunspentCount,listunspentTime';
+  console.log(header)
+
+  // Wait for promises to finish
+  await Promise.all(socketPromises);
+
+  // Print summary
+  if (!quiet) {
+    console.error(`\nAddresses tested: ${addrTotal}`);
+    console.error(`Addresses errored: ${addrErr.length}\n`);
+  }
+
+  // Close sockets
+  for (let i = 0; i < socketPool.length; i++) {
+    await socketPool[i].close()
+  }
 }
 main()
 // EOF
